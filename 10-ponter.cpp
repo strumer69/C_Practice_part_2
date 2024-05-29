cbe6cb49d54e0bd08bfd8271ed004819d1d3e067
shahram <shmoradpour69@gmail.com>
2024-05-29 17:14:21 +0200

    ponter second simple example

--- 10-ponter.cpp
+++ 10-ponter.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main(){
     int x=20;
-    int *pntrx= &x;
+    int *pntrx= &x; //pass the address of x to me not the value
     int z = *pntrx;
-    cout << z<<endl;
+    cout << z <<endl;
+    cout<<  pntrx<<endl;
+    cout << *pntrx <<endl;
+    cout << &*pntrx<<endl;
+
+    //example 2
+    int a=18 , *p;
+    p=&a;
+    cout <<" address of a" <<p <<endl;
+    cout << " mayahtavi address a, which is value of a:" << *p<<endl;
+    cout << "address of the pointer of the a  which is adress of p:" << &p <<endl;
+
 }
